8130ec10ed15cfc0dce1c8b35c20cf8f291d7f30
Jaynald Magbanlag <100130645+Jaynald24@users.noreply.github.com>
2022-02-21 21:21:35 +0700

    Update index.php

--- LT2/index.php
+++ LT2/index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Jaynald Magbanlag</h1>
     <ul>
         <li><a href="index.php">HOME</a></li>
-        <li><a href="recipes.html">LEARNING TASK 2</a></li>
+        <li><a href="Recipes.html">LEARNING TASK 2</a></li>
     </ul>
     </body>
  </html>

c41ed792077a856a8cfaa794c0198b9d11c9c682
Jaynald Magbanlag <100130645+Jaynald24@users.noreply.github.com>
2022-03-24 18:13:45 +0700

    Update index.php

--- CLE1MT/index.php
+++ CLE1MT/index.php
@@ -7,9 +7,9 @@
     <h1>Welcome to My Website! I am Jaynald Magbanlag :)</h1>
     <ul>
        <li><a href="index.html">Home</a> </li>
-		<li><a href="solutions.html">Solutions</a> </li>
-		<li><a href="gallery.html">Gallery</a> </li>
-		<li><a href="aboutus.html">About Us</a> </li>
+		<li><a href="Solutions.html">Solutions</a> </li>
+		<li><a href="Gallery.html">Gallery</a> </li>
+		<li><a href="AboutUs.html">About Us</a> </li>
 		</ul>
     </body>
  </html>

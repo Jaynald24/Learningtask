9276060d7cd2d5c0dbdf57ca777822a5d5936d5f
Jaynald Magbanlag <100130645+Jaynald24@users.noreply.github.com>
2022-03-24 18:11:22 +0700

    Create index.php

---
+++ CLE1MT/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am Jaynald Magbanlag :)</h1>
+    <ul>
+       <li><a href="index.html">Home</a> </li>
+		<li><a href="solutions.html">Solutions</a> </li>
+		<li><a href="gallery.html">Gallery</a> </li>
+		<li><a href="aboutus.html">About Us</a> </li>
+		</ul>
+    </body>
+ </html>
